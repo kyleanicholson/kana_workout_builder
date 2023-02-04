96c6c5373e077fb4a8d864fda21e248e8c422ed7
CodeWithKyle <kyleanicholson@gmail.com>
2023-02-03 20:55:42 -0800

    Create users app, add login logout

--- kana_workout_builder/settings.py
+++ kana_workout_builder/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     "kana_workout_builder_app",
+    "users",
     # Django preinstalled apps
     "django.contrib.admin",
     "django.contrib.auth",
