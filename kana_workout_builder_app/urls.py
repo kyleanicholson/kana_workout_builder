d71fc78ac9b0f40dd644838806cf97e7eb7eb127
knichols <kyleanicholson@gmail.com>
2023-03-20 14:18:33 -0700

    New program model/restructure + new static pgs

--- kana_workout_builder_app/urls.py
+++ kana_workout_builder_app/urls.py
@@ -7,9 +7,20 @@ urlpatterns = [
     # Home page
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("workouts/", views.workouts, name="workouts"),
     path("workouts/<int:workout_id>/", views.workout, name="workout"),
-    path("new_workout/", views.new_workout, name="new_workout"),
+    path("new_workout/<int:program_id>", views.new_workout, name="new_workout"),
     path("new_exercise/<int:workout_id>/", views.new_exercise, name="new_exercise"),
     path("edit_exercise/<int:exercise_id>/", views.edit_exercise, name="edit_exercise"),
+    path("programs/", views.programs, name="programs"),
+    path("programs/<int:program_id>/", views.program, name="program"),
+    path("new_program/", views.new_program, name="new_program"),
+    path(
+        "programs/delete_program/<int:program_id>",
+        views.delete_program,
+        name="delete_program",
+    ),
 ]
+
+# TODO: edit_workout
